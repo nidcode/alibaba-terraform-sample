276b36b7f6d68217440490b0ffb0b534a72da479
Akifumi Niida <nidstyle3@gmail.com>
2019-02-28 11:41:51 +0900

    firstcommit

---
+++ product/main.tf
@@ -0,0 +1,26 @@
+provider "alicloud" {
+  region = "${var.region}"
+}
+
+data "alicloud_zones" main {
+  available_resource_creation = "VSwitch"
+}
+
+data "alicloud_instance_types" "default" {
+  availability_zone = "${data.alicloud_zones.main.zones.0.id}"
+  cpu_core_count    = 1
+  memory_size       = 2
+}
+
+resource "alicloud_cs_managed_kubernetes" "k8s" {
+  name                  = "${var.cluster-name}"
+  availability_zone     = "${data.alicloud_zones.main.zones.0.id}"
+  new_nat_gateway       = true
+  worker_instance_types = ["${data.alicloud_instance_types.default.instance_types.0.id}"]
+  worker_numbers        = [2]
+  key_name              = "qu"
+  pod_cidr              = "172.20.0.0/16"
+  service_cidr          = "172.21.0.0/20"
+  install_cloud_monitor = true
+  worker_disk_category  = "cloud_efficiency"
+}
---
+++ product/variables.tf
@@ -0,0 +1,7 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "cluster-name" {
+  default = "test-cluster"
+}
