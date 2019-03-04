8cb3bdfaf03ecd1cb9cb3d87e0372d6abff97709
Akifumi Niida <nidstyle3@gmail.com>
2019-03-04 22:37:17 +0900

    not managed kubernetes

--- product/main.tf
+++ product/main.tf
@@ -12,10 +12,11 @@ data "alicloud_instance_types" "default" {
   memory_size       = 2
 }
 
-resource "alicloud_cs_managed_kubernetes" "k8s" {
-  name                  = "${var.cluster-name}"
+resource "alicloud_cs_kubernetes" "main" {
+  name_prefix           = "${var.cluster-name}"
   availability_zone     = "${data.alicloud_zones.main.zones.0.id}"
   new_nat_gateway       = true
+  master_instance_types = ["${data.alicloud_instance_types.default.instance_types.0.id}"]
   worker_instance_types = ["${data.alicloud_instance_types.default.instance_types.0.id}"]
   worker_numbers        = [2]
   key_name              = "qu"
